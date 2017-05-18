594948f78bc9ba798ba52addd4a1ac9052e751c8
matali1 <alicja.tadych@gmail.com>
2017-05-17 23:09:20 -0400

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'g3oe_hgw-v8r5iz_mpk$cnvuguoggmhl7zh)_-+g971$^ji72f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com','localhost']
 
 
 # Application definition
